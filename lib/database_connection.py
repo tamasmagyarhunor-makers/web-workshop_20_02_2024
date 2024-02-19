f836759a8d4ac23ca3c4f58d367286070fb41842
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2024-02-19 15:59:51 +0000

    Add databases

--- lib/database_connection.py
+++ lib/database_connection.py
@@ -11,8 +11,8 @@ from psycopg.rows import dict_row
 # That's why we have provided it!
 class DatabaseConnection:
     # VVV CHANGE BOTH OF THESE VVV
-    DEV_DATABASE_NAME = "DEFAULT_MAKERS_PROJECT"
-    TEST_DATABASE_NAME = "DEFAULT_MAKERS_PROJECT_TEST"
+    DEV_DATABASE_NAME = "book_store_19_02"
+    TEST_DATABASE_NAME = "book_store_19_02_test"
 
     def __init__(self, test_mode=False):
         self.test_mode = test_mode
